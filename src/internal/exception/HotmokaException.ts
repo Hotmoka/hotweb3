da63533ac818018c6d38219335b0d9467d90b3ea
Dinu Berinde <dinu2193@gmail.com>
2021-07-15 21:59:09 +0200

    improving HotmokaException

--- src/internal/exception/HotmokaException.ts
+++ src/internal/exception/HotmokaException.ts
@@ -3,5 +3,6 @@ export class HotmokaException extends Error {
     constructor(message?: string) {
         super(message)
         this.name = "HotmokaException"
+        Object.setPrototypeOf(this, HotmokaException.prototype);
     }
 }
